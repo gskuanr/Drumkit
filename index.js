19f3d72eb755cfc122f3c1fa1ae38e44f050c5e1
gskuanr <girijashankariitg@gmail.com>
2021-07-27 20:36:30 +0530

    Add index.html index.js and style.css and image and sound folders

---
+++ index.js
@@ -0,0 +1,8 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for ( i=0; i < numberOfDrumButtons ; i++) {
+    document.querySelectorAll( "button" )[i].addEventListener( "click", function() {
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    });
+}
