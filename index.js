1dc096a1a48b15c0df066dd13327f212bb76d352
gskuanr <girijashankariitg@gmail.com>
2021-07-28 17:20:01 +0530

    Optimize the detect clicks and keypress code

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+//detecting clicks
 for ( i=0; i < numberOfDrumButtons ; i++) {
     document.querySelectorAll( "button" )[i].addEventListener( "click", function() {
-        console.log(this.innerHTML);
-        switch ( this.innerHTML ) {
+        buttonInnerHTML = this.innerHTML;
 
-            case "w":
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break;
-            
-            case "a":
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
-                break;
-            
-            case "s":
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
-                break;
-            
-            case "d":
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
-                break;
-
-            case "j":
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
-                break;
-
-            case "k":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-
-            case "l":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-                break;
-            
-            default:
-                console.log(this.innerHTML);
-        }
-
+        makeSound ( buttonInnerHTML );
+        animatePress( buttonInnerHTML );
     });
 }
 
+// detecting key board press
 document.addEventListener("keydown", function (event ) {
     var pressedKey = event.key;
     
-    switch ( pressedKey ) {
+    makeSound( pressedKey );
+    animatePress( pressedKey );
+})
+
+function makeSound (key) {
+
+    switch ( key ) {
         case "w":
             var audio = new Audio("sounds/tom-1.mp3");
             audio.play();
@@ -87,6 +57,16 @@ document.addEventListener("keydown", function (event ) {
             break;
         
         default:
-            console.log(pressedKey);
+            console.log(key);
     }
-})
+
+}
+
+function animatePress ( button ) {
+
+document.querySelector("." + button).classList.add("pressed");
+setTimeout( function() {
+    document.querySelector("." + button).classList.remove("pressed");
+}, 200);
+
+}
